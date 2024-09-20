f17e513d51d164b93c7162ab0b29bce357f3926a
Chinoachee <djironov@mail.ru>
2024-09-21 02:18:48 +0400

    Добавлена модель абстрактного класса User

---
+++ DigitalFinanceReactApp.Server/Models/User/AbstractUser.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalFinanceReactApp.Server.Models.User {
+    public abstract class AbstractUser {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        protected AbstractUser(int id,string name) {
+            Id = id;
+            Name = name;
+        }
+        protected AbstractUser(string name) {
+            Name = name;
+        }
+    }
+}
