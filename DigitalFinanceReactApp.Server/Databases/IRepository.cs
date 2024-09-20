fd450ab49ec911a0bac5837fb23177c58c6f7b8a
Chinoachee <djironov@mail.ru>
2024-09-21 02:19:54 +0400

    Добавлен интерфейс реализации репозиториев

---
+++ DigitalFinanceReactApp.Server/Databases/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace DigitalFinanceReactApp.Server.Databases {
+    public interface IRepository<T> where T : class {
+        T? Get();
+        int Add(T entity);
+        T? GetById(int id);
+        int Delete(T entity);
+        int Update(T entity);
+    }
+}
