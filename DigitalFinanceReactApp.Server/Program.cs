84224dd6f5ae60d707ef1b3ee0e85839d03a0515
Chinoachee <djironov@mail.ru>
2024-09-21 02:17:06 +0400

    Добавлен контекст работы с базой данной PostgreSQL на основе ORM системы Dapper

--- DigitalFinanceReactApp.Server/Program.cs
+++ DigitalFinanceReactApp.Server/Program.cs
@@ -1,7 +1,12 @@
+using Npgsql;
+using System.Data;
+
 namespace DigitalFinanceReactApp.Server {
     public class Program {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<IDbConnection>(db => new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddAuthorization();
 
