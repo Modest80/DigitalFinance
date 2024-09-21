24153a1bef7604ca94bf26da090818125430392d
Aleksandr <110898Belov@mail.ru>
2024-09-21 16:40:03 +0400

    Создан SQL для таблицы User

---
+++ DigitalFinanceReactApp.Server/Querys/UserQuery.sql
@@ -0,0 +1,7 @@
+﻿create table Users(
+	id SERIAL primary key,
+	name VARCHAR(50) not NULL,
+	email VARCHAR(100) unique not NULL,
+	phone VARCHAR(30) unique not NULL,
+	password VARCHAR(64) not NULL
+)
