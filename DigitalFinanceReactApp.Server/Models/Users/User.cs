dc1734db54d5d41dbb762070bce55b964f089b00
modest80 <modesting@gmail.com>
2024-09-23 20:27:11 +0400

    Обновление UsersController

--- DigitalFinanceReactApp.Server/Models/Users/User.cs
+++ DigitalFinanceReactApp.Server/Models/Users/User.cs
@@ -2,9 +2,9 @@
 
 namespace DigitalFinanceReactApp.Server.Models.Users {
     public class User : AbstractUser {
-        public string? Email;
-        public string? Phone;
-        public string? Password;
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
         public User(int id, string? name, string? email, string? phone, string? password):
             base (id, name) {
             Email = email;
