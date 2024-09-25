6cce65f3c474635edba143ece012dcb89a080147
Aleksandr <110898Belov@mail.ru>
2024-09-25 23:35:47 +0400

    Create model, repository, controller TypeAccount

---
+++ DigitalFinanceReactApp.Server/Controllers/TypesAccountsController.cs
@@ -0,0 +1,36 @@
+﻿using DigitalFinanceReactApp.Server.Databases;
+using DigitalFinanceReactApp.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace DigitalFinanceReactApp.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TypesAccountsController : ControllerBase
+	{
+		private readonly IRepository<TypeAccount> _typeRepository;
+        public TypesAccountsController(IRepository<TypeAccount> repository)
+        {
+            _typeRepository = repository;
+        }
+		[HttpGet]
+		public ActionResult Get()
+		{
+			try
+			{
+				var type = _typeRepository.Get();
+				if(type == null) 
+				{
+					return NotFound("По запросу ничего не найдено");
+				}
+				return Ok(type);
+			}
+			catch (Exception)
+			{
+				return StatusCode(500, "Ошибка сервера");
+			}
+		}
+    }
+}
---
+++ DigitalFinanceReactApp.Server/Databases/TypeAccountRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using DigitalFinanceReactApp.Server.Models;
+using System.Data;
+
+namespace DigitalFinanceReactApp.Server.Databases
+{
+	public class TypeAccountRepository : Repository, IRepository<TypeAccount>
+	{
+		public TypeAccountRepository(IDbConnection dbConnection) : base(dbConnection) { }
+
+		public int Add(TypeAccount entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<TypeAccount>? Get()
+		{
+			var query = "SELECT * FROM All_Accounts;";
+			return _connection.Query<TypeAccount>(query).ToList();
+		}
+
+		public TypeAccount? GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Update(TypeAccount entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
