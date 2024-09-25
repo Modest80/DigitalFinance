a439cf9fe505333fe3a9bc20a2d5845fbc0cc103
Aleksandr <110898Belov@mail.ru>
2024-09-25 20:09:24 +0400

    Добавлен запрос для PostgreSQL

--- DigitalFinanceReactApp.Server/Querys/CreateTables.sql
+++ DigitalFinanceReactApp.Server/Querys/CreateTables.sql
@@ -27,3 +27,34 @@ CREATE TABLE Histories (
     Commentary VARCHAR(1024) NOT NULL,
     Created_at bigint DEFAULT extract(epoch from now())
 );
+
+-- Создание всех счетов (для гибкости)
+CREATE TABLE All_Accounts(
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(50) NOT NULL
+);
+
+--Заполняем счета в таблицу
+INSERT INTO All_Accounts(type) VALUES
+('Сберегательный'),
+('Текущий'),
+('Кредитный');
+
+--Запускаем последовательность для уникального номера счета(каждый раз будет +1)
+-- PS Создавать последовательность нужно до создания таблицы
+--CREATE SEQUENCE account_number_reg START 1000000000; 
+
+---- Редактирование таблицы Accounts
+--CREATE TABLE Accounts (
+--    id SERIAL PRIMARY KEY,
+--    user_id int REFERENCES Users(id),
+--    title Text,
+--    account_number_reg VARCHAR(20) NOT NULL UNIQUE DEFAULT (nextval('account_number_seq')::text),
+--    balance decimal NOT NULL DEFAULT 0.0,
+--    created_at bigint NOT NULL,
+--    updated_at bigint NOT NULL,
+--    status VARCHAR(50) NOT NULL DEFAULT 'Created',
+--    accounts_id int,
+
+--    FOREIGN KEY accounts_id REFERENCES  All_Accounts(id)
+--);
